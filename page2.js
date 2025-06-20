f3c3ef5c494137cd546a29e3f6636bee6e6048c6
Antony Reis <antonyyyno@gmail.com>
2025-06-20 14:19:46 -0300

    Adicionando o projeto Flask

--- page2.js
+++ page2.js
@@ -1,10 +1,12 @@
 // 1. Referências aos cards
 const cardHIV  = document.getElementById('hiv-card');
 const cardSite = document.getElementById('site-card');
+const cardFlask = document.getElementById('flask-card');
 
 // 2. Referências aos modais
 const modalHIV  = document.getElementById('hiv-text');
 const modalSite = document.getElementById('site_text');
+const modalFlask = document.getElementById('flask-text');
 
 // 3. Abrir modais ao clicar nos cards
 cardHIV.addEventListener('click', () => {
@@ -12,6 +14,9 @@ cardHIV.addEventListener('click', () => {
 });
 cardSite.addEventListener('click', () => {
   modalSite.style.display = 'inline-flex';
+});
+cardFlask.addEventListener('click', () => {
+  modalFlask.style.display = 'inline-flex';
 });
 
 // 4. Função para fechar um modal
@@ -25,3 +30,4 @@ function attachClose(modalEl) {
 // 5. Vincula o fechamento em cada modal
 attachClose(modalHIV);
 attachClose(modalSite);
+attachClose(modalFlask);
